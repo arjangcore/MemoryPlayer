5610499b3157aafc17f2ae6cbb3a420f4b936245
Reza Nezami <arjangcore@gmail.com>
2015-12-14 00:52:56 -0800

    testing comment again

--- MemoryLogItem.cs
+++ MemoryLogItem.cs
@@ -7,7 +7,7 @@ namespace MemoryPlayer
 	/// A single memory log entry, like allocate, free or label.
 	/// </summary>
     public class MemoryLogItem
-	{ // comment...
+	{ // comment... a second comment
         public enum ItemType { Alloc, Free, Label, Size }
         private static String[] ItemTypeName = {"ALLOC", "FREE", "LABEL", "SIZE"};
 
